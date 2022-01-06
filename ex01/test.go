9f1423c9215aa10fbb01f0d24124fde37b4eaf40
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2022-01-06 16:38:23 +0900

    testfile作成

--- ex01/test.go
+++ ex01/test.go
@@ -6,29 +6,46 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 16:11:04 by hryuuta           #+#    #+#             */
-/*   Updated: 2022/01/05 16:37:18 by hryuuta          ###   ########.fr       */
+/*   Updated: 2022/01/06 16:19:22 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"io"
+	"os"
 )
 
 func main() {
-	url := "https://4.bp.blogspot.com/-2-Ny23XgrF0/Ws69gszw2jI/AAAAAAABLdU/unbzWD_U8foWBwPKWQdGP1vEDoQoYjgZwCLcBGAs/s1600/top_banner.jpg"
-	resp, err := http.Get(url)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-	fmt.Println(resp.Header.Get("Authorization"))
-	resp.Header.Set("Authorization", "Bearer access-token")
-	fmt.Println(resp.Header.Get("Authorization"))
-	resp.Header.Set("Authorization", "BBBBBB access-token")
-	fmt.Println(resp.Header.Get("Authorization"))
-	//	fmt.Println(resp)
+	srcName := os.Args[1]
+	src2Name := os.Args[2]
+	dstName := os.Args[3]
 
+	src1, err := os.Open(srcName)
+	if err != nil {
+		panic(err)
+	}
+	defer src1.Close()
+
+	src2, err := os.Open(src2Name)
+	if err != nil {
+		panic(err)
+	}
+	defer src2.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		panic(err)
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src1)
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(dst, src2)
+	if err != nil {
+		panic(err)
+	}
 }
